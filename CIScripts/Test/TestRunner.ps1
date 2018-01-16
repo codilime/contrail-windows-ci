f38711dfdd0bc6c9b817e62691d6f8503340fdda
Michal Kostrzewa <michal.kostrzewa@codilime.com>
2018-01-16 15:49:45 +0100

    TestConfiguration from file

--- CIScripts/Test/TestRunner.ps1
+++ CIScripts/Test/TestRunner.ps1
@@ -21,67 +21,69 @@ function Run-TestScenarios {
 
     $Job.Step("Running all integration tests", {
 
-        $SingleSubnetNetworkConfiguration = [NetworkConfiguration] @{
-            Name = $Env:SINGLE_SUBNET_NETWORK_NAME
-            Subnets = @($Env:SINGLE_SUBNET_NETWORK_SUBNET)
-        }
+        # $SingleSubnetNetworkConfiguration = [NetworkConfiguration] @{
+        #     Name = $Env:SINGLE_SUBNET_NETWORK_NAME
+        #     Subnets = @($Env:SINGLE_SUBNET_NETWORK_SUBNET)
+        # }
 
-        $MultipleSubnetsNetworkConfiguration = [NetworkConfiguration] @{
-            Name = $Env:MULTIPLE_SUBNETS_NETWORK_NAME
-            Subnets = @($Env:MULTIPLE_SUBNETS_NETWORK_SUBNET1, $Env:MULTIPLE_SUBNETS_NETWORK_SUBNET2)
-        }
+        # $MultipleSubnetsNetworkConfiguration = [NetworkConfiguration] @{
+        #     Name = $Env:MULTIPLE_SUBNETS_NETWORK_NAME
+        #     Subnets = @($Env:MULTIPLE_SUBNETS_NETWORK_SUBNET1, $Env:MULTIPLE_SUBNETS_NETWORK_SUBNET2)
+        # }
 
-        $NetworkWithPolicy1Configuration = [NetworkConfiguration] @{
-            Name = $Env:NETWORK_WITH_POLICY_1_NAME
-            Subnets = @($Env:NETWORK_WITH_POLICY_1_SUBNET)
-        }
+        # $NetworkWithPolicy1Configuration = [NetworkConfiguration] @{
+        #     Name = $Env:NETWORK_WITH_POLICY_1_NAME
+        #     Subnets = @($Env:NETWORK_WITH_POLICY_1_SUBNET)
+        # }
 
-        $NetworkWithPolicy2Configuration = [NetworkConfiguration] @{
-            Name = $Env:NETWORK_WITH_POLICY_2_NAME
-            Subnets = @($Env:NETWORK_WITH_POLICY_2_SUBNET)
-        }
+        # $NetworkWithPolicy2Configuration = [NetworkConfiguration] @{
+        #     Name = $Env:NETWORK_WITH_POLICY_2_NAME
+        #     Subnets = @($Env:NETWORK_WITH_POLICY_2_SUBNET)
+        # }
 
-        $TenantConfiguration = [TenantConfiguration] @{
-            Name = $Env:DOCKER_NETWORK_TENANT_NAME;
-            DefaultNetworkName = $SingleSubnetNetworkConfiguration.Name;
-            SingleSubnetNetwork = $SingleSubnetNetworkConfiguration;
-            MultipleSubnetsNetwork = $MultipleSubnetsNetworkConfiguration;
-            NetworkWithPolicy1 = $NetworkWithPolicy1Configuration;
-            NetworkWithPolicy2 = $NetworkWithPolicy2Configuration;
-        }
+        # $TenantConfiguration = [TenantConfiguration] @{
+        #     Name = $Env:DOCKER_NETWORK_TENANT_NAME;
+        #     DefaultNetworkName = $SingleSubnetNetworkConfiguration.Name;
+        #     SingleSubnetNetwork = $SingleSubnetNetworkConfiguration;
+        #     MultipleSubnetsNetwork = $MultipleSubnetsNetworkConfiguration;
+        #     NetworkWithPolicy1 = $NetworkWithPolicy1Configuration;
+        #     NetworkWithPolicy2 = $NetworkWithPolicy2Configuration;
+        # }
 
-        $DockerDriverConfiguration = [DockerDriverConfiguration] @{
-            Username = $Env:DOCKER_DRIVER_USERNAME;
-            Password = $Env:DOCKER_DRIVER_PASSWORD;
-            AuthUrl = $Env:DOCKER_DRIVER_AUTH_URL;
-            TenantConfiguration = $TenantConfiguration;
-        }
+        # $DockerDriverConfiguration = [DockerDriverConfiguration] @{
+        #     Username = $Env:DOCKER_DRIVER_USERNAME;
+        #     Password = $Env:DOCKER_DRIVER_PASSWORD;
+        #     AuthUrl = $Env:DOCKER_DRIVER_AUTH_URL;
+        #     TenantConfiguration = $TenantConfiguration;
+        # }
 
-        $TestConfiguration = [TestConfiguration] @{
-            ControllerIP = $Env:CONTROLLER_IP;
-            ControllerRestPort = 8082
-            ControllerHostUsername = $Env:CONTROLLER_HOST_USERNAME;
-            ControllerHostPassword = $Env:CONTROLLER_HOST_PASSWORD;
-            AdapterName = $Env:ADAPTER_NAME;
-            VMSwitchName = "Layered " + $Env:ADAPTER_NAME;
-            VHostName = "vEthernet (HNSTransparent)"
-            ForwardingExtensionName = $Env:FORWARDING_EXTENSION_NAME;
-            AgentConfigFilePath = "C:\ProgramData\Contrail\etc\contrail\contrail-vrouter-agent.conf";
-            DockerDriverConfiguration = $DockerDriverConfiguration;
-            LinuxVirtualMachineIp = $Env:LINUX_VIRTUAL_MACHINE_IP;
-        }
+        # $TestConfiguration = [TestConfiguration] @{
+        #     ControllerIP = $Env:CONTROLLER_IP;
+        #     ControllerRestPort = 8082
+        #     ControllerHostUsername = $Env:CONTROLLER_HOST_USERNAME;
+        #     ControllerHostPassword = $Env:CONTROLLER_HOST_PASSWORD;
+        #     AdapterName = $Env:ADAPTER_NAME;
+        #     VMSwitchName = "Layered " + $Env:ADAPTER_NAME;
+        #     VHostName = "vEthernet (HNSTransparent)"
+        #     ForwardingExtensionName = $Env:FORWARDING_EXTENSION_NAME;
+        #     AgentConfigFilePath = "C:\ProgramData\Contrail\etc\contrail\contrail-vrouter-agent.conf";
+        #     DockerDriverConfiguration = $DockerDriverConfiguration;
+        #     LinuxVirtualMachineIp = $Env:LINUX_VIRTUAL_MACHINE_IP;
+        # }
 
-        $SNATConfiguration = [SNATConfiguration] @{
-            EndhostIP = $Env:SNAT_ENDHOST_IP;
-            VethIP = $Env:SNAT_VETH_IP;
-            GatewayIP = $Env:SNAT_GATEWAY_IP;
-            ContainerGatewayIP = $Env:SNAT_CONTAINER_GATEWAY_IP;
-            EndhostUsername = $Env:SNAT_ENDHOST_USERNAME;
-            EndhostPassword = $Env:SNAT_ENDHOST_PASSWORD;
-            DiskDir = $Env:SNAT_DISK_DIR;
-            DiskFileName = $Env:SNAT_DISK_FILE_NAME;
-            VMDir = $Env:SNAT_VM_DIR;
-        }
+        $TestConfiguration = Import-LocalizedData -FileName TestConfiguration.psd1
+
+        # $SNATConfiguration = [SNATConfiguration] @{
+        #     EndhostIP = $Env:SNAT_ENDHOST_IP;
+        #     VethIP = $Env:SNAT_VETH_IP;
+        #     GatewayIP = $Env:SNAT_GATEWAY_IP;
+        #     ContainerGatewayIP = $Env:SNAT_CONTAINER_GATEWAY_IP;
+        #     EndhostUsername = $Env:SNAT_ENDHOST_USERNAME;
+        #     EndhostPassword = $Env:SNAT_ENDHOST_PASSWORD;
+        #     DiskDir = $Env:SNAT_DISK_DIR;
+        #     DiskFileName = $Env:SNAT_DISK_FILE_NAME;
+        #     VMDir = $Env:SNAT_VM_DIR;
+        # }
 
         Test-AgentService -Session $Sessions[0] -TestConfiguration $TestConfiguration
         Test-ExtensionLongLeak -Session $Sessions[0] -TestDurationHours $Env:LEAK_TEST_DURATION -TestConfiguration $TestConfiguration
