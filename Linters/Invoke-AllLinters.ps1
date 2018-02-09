94787a99741153de1ce184e127ce3da398a3b11b
Michal Kostrzewa <michal.kostrzewa@codilime.com>
2018-02-09 18:16:52 +0100

    Remove another bad import.

--- Linters/Invoke-AllLinters.ps1
+++ Linters/Invoke-AllLinters.ps1
@@ -1,8 +1,6 @@
 Param([Parameter(Mandatory = $true)] [string] $RootDir,
       [Parameter(Mandatory = $true)] [string] $ConfigDir)
 
-. $PSScriptRoot\Invoke-PowershellLinter.ps1
-
 $PSLinterConfig = "$ConfigDir/PSScriptAnalyzerSettings.psd1"
 $Output = Invoke-ScriptAnalyzer $RootDir -Recurse -Setting $PSLinterConfig `
       -ErrorAction Continue -WarningAction Continue
