d87f3f02d5bfd66b153525cab47842f0a1df9bdf
Michal Kostrzewa <michal.kostrzewa@codilime.com>
2018-02-19 14:52:35 +0100

    Print path to linter config.

--- Linters/Invoke-AllLinters.ps1
+++ Linters/Invoke-AllLinters.ps1
@@ -1,14 +1,17 @@
 Param([Parameter(Mandatory = $true)] [string] $RootDir,
       [Parameter(Mandatory = $true)] [string] $ConfigDir)
 
-Write-Host "Running PSScriptAnalyzer..."
-
 $PSLinterConfig = "$ConfigDir/PSScriptAnalyzerSettings.psd1"
+Write-Host "Running PSScriptAnalyzer... (config from $PSLinterConfig)"
+
 $Output = Invoke-ScriptAnalyzer $RootDir -Recurse -Setting $PSLinterConfig `
       -ErrorAction Continue -WarningAction Continue
 if ($Output) {
       Write-Host ($Output | Format-Table | Out-String)
       exit 1
 }
+
+# test
+$a = 0
 
 exit 0
