118ddf94d77f47dae5824087774bd93f75b969a2
Katarzyna Rybacka <katarzyna.rybacka@codilime.com>
2018-02-26 13:31:26 +0100

    renamed playbook

--- vars/provisionContrailController.groovy
+++ vars/provisionContrailController.groovy
@@ -1,6 +1,6 @@
 def call(vmwareConfig, controllerIP) {
     dir('ansible') {
-        ansiblePlaybook  playbook: 'CentOS-provision.yml',
+        ansiblePlaybook  playbook: 'controllerProvision.yml',
                          extraVars: controllerIP,
                          extras: '-e @vmware-vm.vars'
     }
