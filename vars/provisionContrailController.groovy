e29e7a1f797fd1882ce698fafa4b77fccf434d19
Pawel Marchewka <pawel.marchewka@codilime.com>
2018-02-28 09:05:38 +0100

    Some of CR fixes

--- vars/provisionContrailController.groovy
+++
@@ -1,7 +0,0 @@
-def call(vmwareConfig, controllerIP) {
-    dir('ansible') {
-        ansiblePlaybook  playbook: 'controllerProvision.yml',
-                         extraVars: controllerIP,
-                         extras: '-e @vmware-vm.vars'
-    }
-}
